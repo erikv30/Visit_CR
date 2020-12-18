4c06a2671d90987d57b81e47725ebfae515b4c85
Erick Vega <erikvega30@hotmail.com>
2020-12-18 04:06:56 -0600

    ScrollToTop added

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ import Actividades from './components/Pages/Actividades';
 import Servicios from './components/Pages/Servicios';
 import SignUp from './components/Pages/SignUp';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <>
-    <Router>
+    <Router basename='/'>
+      <ScrollToTop />
       <Navbar />
       <Switch>
-        <Route path='/' exact component={Inicio} />
+        <Route exact path='/' component={Inicio} />
         <Route path='/costa-rica' component={CostaRica} />
         <Route path='/actividades' component={Actividades} />
         <Route path='/servicios' component={Servicios} />
